a8c1adca54c58d582be54be654ee01086ab44fa3
npbreland <34611559+npbreland@users.noreply.github.com>
2021-03-11 13:34:58 +0100

    Add file via upload

---
+++ RecipeVeganizer.php
@@ -0,0 +1,257 @@
+<?php
+
+
+class RecipeVeganizer {
+    
+    public static $subsDictionary = [
+        'butter' => [
+            'replaceStr' => 'vegan butter',
+            'ratio' => 1
+        ],
+    
+        'eggs' => [
+            'replaceStr' => 'flax eggs',
+            'ratio' => 1
+        ],
+    
+        'egg' => [
+            'replaceStr' => 'flax egg',
+            'ratio' => 1
+        ],
+    
+        'milk' => [
+            'replaceStr' => 'non-dairy milk (soy, almond, oat, etc.)',
+            'ratio' => 2
+        ],
+    
+        'cream' => [
+            'replaceStr' => 'coconut cream',
+            'ratio' => 2
+        ],
+    
+        'heavy cream' => [
+            'replaceStr' => 'coconut cream',
+            'ratio' => 2
+        ],
+    
+        'honey' => [
+            'replaceStr' => 'maple syrup/agave nectar',
+            'ratio' => 1
+        ]
+    ];
+
+    public static $units = [
+        'ounces', 
+        'oz.', 
+        'fluid ounces',
+        'fl oz',
+        'fl. oz.',
+        'fl.oz.',
+    
+        'teaspoons',
+        'teaspoon',
+        'tsp',
+        'tablespoons',
+        'tablespoon',
+        'Tbsp',
+        'T',
+        
+        'ml',
+        'milliliter',
+        'milliliters',
+        'L', 
+        'liter',
+        'liters',
+        'cup',
+        'cups', 
+        'C',
+        'pint',
+        'pt.',
+        'pt',
+        'quart',
+        'qt.',
+        'qt',
+        'gallon',
+        'gallons',
+    
+        'stick',
+        'sticks',
+        'g', 
+        'gram',
+        'grams', 
+    ];
+
+    public function roundToCommonFractions($decimal)
+    {
+        if ($decimal < 0.125) {
+            return 0;
+        }
+
+        if ($decimal >= 0.125 && $decimal < 0.29) {
+            $fraction = '1/4';
+        } else if ($decimal < 0.415) {
+            $fraction = '1/3';
+        } else if ($decimal < 0.58) {
+            $fraction = '1/2';
+        } else if ($decimal < 0.705) {
+            $fraction = '2/3';
+        } else if ($decimal < 0.875) {
+            $fraction = '3/4';
+        } else {
+            $fraction = 1;
+        }
+
+        return $fraction;
+    }
+
+    public function veganize($recipeToVeganize)
+    {
+        // Split input string into lines
+        $recipeLines = preg_split('/(\n|\r)/', $recipeToVeganize, -1, PREG_SPLIT_NO_EMPTY);
+
+        // Split lines into qty and item pieces
+        $recipeLines = array_map(function($line) {
+            
+            $pieces = preg_split('/^(\d+\/\d+|\d+(\s\d+\/\d+)?)/', $line, -1, PREG_SPLIT_OFFSET_CAPTURE | PREG_SPLIT_NO_EMPTY);
+
+            // Only get the first match
+            $str = trim($pieces[0][0]);
+            $offset = $pieces[0][1];
+
+            $qty = substr($line, 0, $offset);
+            
+            return [ 'qty' => $qty, 'item' => $str ]; 
+        }, $recipeLines);
+
+
+        // Convert the mixed numbers to decimals so we can do math.
+        $recipeLines = array_map(function($line) {
+
+            $qty = $line['qty'];
+
+            preg_match('/\d+\/\d+/', $qty, $matches, PREG_OFFSET_CAPTURE);
+
+            // If no matches, continue on.
+            if (count($matches) === 0) {
+                return $line;
+            }
+
+            // Only get the first match
+            $match = $matches[0][0];
+            $offset = $matches[0][1];
+
+            $decimal = 0;
+            // The fraction is offset, which means we have a whole number
+            if ($offset > 0) {
+                $decimal = intval(substr($qty, 0, $offset));
+            }
+
+            $fractionPieces = explode('/', $match);
+            $numer = $fractionPieces[0]; 
+            $denom = $fractionPieces[1];
+
+            $decimal += $numer / $denom;
+
+            $line['qty'] = $decimal;
+
+            return $line;
+
+        }, $recipeLines);
+
+        // Further split item string into unit and item pieces
+        $recipeLines = array_map(function($line){
+            $itemStr = $line['item'];
+            foreach (self::$units as $unit) {
+                
+                $pieces = preg_split("/^($unit)\s+/", $itemStr, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+
+                if ($pieces[1]) {
+                    return [
+                        'qty' => $line['qty'],
+                        'unit' => trim($pieces[0]),
+                        'item' => trim($pieces[1])
+                    ];
+                }
+                
+            }
+
+            $line['unit'] = '';
+            return $line;
+            
+        }, $recipeLines);
+
+        // Do the replacement and adjust the quantities
+        $recipeLines = array_map(function($line){
+
+            $itemStr = $line['item'];
+            $qty = $line['qty'];
+
+            foreach (self::$subsDictionary as $toSub => $valArr) {
+                $subTo = $valArr['replaceStr'];
+                $matchFound = preg_match("/$toSub.*/", $itemStr);
+                if ($matchFound) {
+                    // We replaced something. 
+                    $ratio = $valArr['ratio'];
+                    $newQty = $qty * $ratio;
+
+                    return [
+                        'qty' => $newQty,
+                        'unit' => $line['unit'],
+                        'item' => $subTo
+                    ];
+                }
+            }
+
+            // We didn't find a match - so just return it unchanged.
+            return $line;
+
+        }, $recipeLines);
+
+        // Convert decimals back to fractions
+        $recipeLines = array_map(function($line) {
+
+            $qty = $line['qty'];
+            
+            if (!$qty) {
+                // No qty value, so skip
+                return $line;
+            }
+
+            $qtyWhole = floor($qty);
+            $qtyDecimal = $qty - $qtyWhole;
+
+            // Convert to fraction notation.
+            $fraction = $this->roundToCommonFractions($qtyDecimal);
+
+            if (!$qtyWhole) {
+                // If no whole number to add it to, just use the fraction
+                $line['qty'] = $fraction;
+                return $line;
+            }
+
+            if (gettype($fraction) === 'string') {
+                // It's a fraction, so concat it to the whole number
+                $qty = $qtyWhole . ' ' . $fraction;
+            } else {
+                // It's an int, so add it to the whole number.
+                $qty = $qtyWhole + $fraction;
+            }
+
+            $line['qty'] = $qty;
+            return $line;
+
+        }, $recipeLines);
+
+        // Finally, let's build the response HTML
+        $recipeLines = array_map(function($line){
+            return implode(' ', [ $line['qty'], $line['unit'], $line['item'] ]);
+        }, $recipeLines);
+
+        // Add html line break characters.
+        $responseText = implode('&#13;&#10;', $recipeLines);
+
+        return $responseText;
+    }
+}
+
+
